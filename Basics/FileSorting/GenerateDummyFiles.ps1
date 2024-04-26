68fcb08cee0a6f73f954935a8f922e8c43ec12bd
yoonsii <118307793+yoonsii@users.noreply.github.com>
2024-04-26 22:44:04 +1200

    Create GenerateDummyFiles.ps1

---
+++ Basics/FileSorting/GenerateDummyFiles.ps1
@@ -0,0 +1,17 @@
+# Set the path to the directory where you want to create the dummy files
+$directory = "/Users/yoonsi/Documents/PowerShell-Kata"
+
+# Create 10 dummy files
+1..10 | ForEach-Object {
+    # Generate a random file name
+    $fileName = "DummyFile$_" + (Get-Random -Minimum 1000 -Maximum 9999)
+    
+    # Generate a random file extension from a list of extensions
+    $extensions = @(".txt", ".docx", ".xlsx", ".pdf")
+    $extension = $extensions | Get-Random
+    
+    # Create the dummy file
+    $null = New-Item -Path "$directory\$fileName$extension" -ItemType File
+}
+
+Write-Host "Dummy files created in $directory"
