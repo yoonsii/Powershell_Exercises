1a65bf4478f5770566407f91171160942f3fd107
Keshan Pillay <kpillay@hotmail.co.nz>
2024-04-30 20:05:40 +1200

    Completes Script

--- Basics/FileSorting/ExerciseSortingFiles.ps1
+++ Basics/FileSorting/ExerciseSortingFiles.ps1
@@ -1,27 +1,40 @@
 function Insert-File {
     param (
-        $folderName
+        $folderName,
+        $file
     )
 
     if(Test-Path -Path $path/$folderName) {
-        "Path Exists"
+        Move-Item -Path "$path/$file.$folderName" -Destination $path/$folderName/
+        Write-Host "Moving File $path/$file.$folderName to $path/$folderName/" 
     } else {
-        "Does not exist!"
+        Write-Host "Creating Folder: $folderName"
+        New-Item -Path $path -Name $folderName -ItemType "Directory"
+        Move-Item -Path "$path/$file.$folderName" -Destination $path/$folderName/
+        Write-Host "Moving File $path/$file.$folderName to $path/$folderName/" 
     }
-
 }
 
 $path = Read-Host -prompt "Put placeholder text here" #TODO : Change this to path later
 
 $path = '/Users/yoonsi/Documents/Projects/Powershell_Exercises/Basics/FileSorting/'
 
-Write-host $path 
+#Write-host $path 
 
 Get-ChildItem -Path $path | ForEach-Object -Process {
-    Write-Host $PSItem.Name.Split('.')[1] 
+    $filename = $PSItem.Name.Split('.')[0] 
+    $extension = $PSItem.Name.Split('.')[1] 
+
+    #$filename
+    #$extension
+
+    if ($extension -eq "txt" -or $extension -eq "docx" -or $extension -eq "xlsx" -or $extension -eq "pdf") {
+        Insert-File -folderName $extension -file $filename
+        #TODO : Output to a log file
+    }
 
 }
 
-Insert-File -folderName Test
+
 
 
